f4262113fed4fb8199daf2a47e90a8fe291a3cbc
Rui Dias de Oliveira Filho <rui.dias@CTNDELL2CHF0D3.pampa.compasso>
2021-08-16 11:33:11 -0300

    implementacao do usuario e da validacao na insercao do mesmo no banco de dados

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 
 const rotaProdutos = require('./routes/produtos');
 const rotaPedidos = require('./routes/pedidos');
+const rotaUsuarios = require('./routes/usuarios');
 
 app.use(morgan('dev'));
 app.use('/uploads', express.static('uploads'));
@@ -28,6 +29,7 @@ app.use((req,res,next) => {
 
 app.use('/produtos', rotaProdutos);
 app.use('/pedidos', rotaPedidos);
+app.use('/usuarios', rotaUsuarios);
 
 app.use((req, res, next) =>{
     const erro = new Error('Não encontrado');
